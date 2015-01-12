40fd4ceb8a452f621164b4a6a4586ff2b6cc9081
Daniel Stepp <willstepp@gmail.com>
2015-01-12 10:18:46 -0500

    added bower for front-end package management

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -8,9 +8,9 @@ var rename = require('gulp-rename');
 var inlineTemplates = require('gulp-inline-angular-templates');
 
 var libSource = [
-  'app/js/libraries/angular/angular.js',
-  'app/js/libraries/angular/angular-route.js',
-  'app/js/libraries/jquery/jquery.js'
+  'bower_components/angular/angular.js',
+  'bower_components/angular-route/angular-route.js',
+  'bower_components/jquery/dist/jquery.js'
 ];
 
 var appSource = [
